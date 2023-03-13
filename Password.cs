595a6868f9e059deb381f121f622767abee7a062
Lars482 <lucaslarsen@hotmail.dk>
2023-03-13 15:35:13 +0100

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace MySecondProject
+{
+	class Password
+	{
+		string passwordChar = "njchsjs8w882jsjsn????wjjnsj";
+
+		Random rand = new Random();
+        private object randomChar = "";
+
+    
+        public string passwordGen(int length)
+		{
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                char randomChar = passwordChar[rand.Next(0, passwordChar.Length)];
+                sb.Append(randomChar);
+              
+            }
+
+            
+            return sb.ToString();
+            
+
+
+        }
+		
+    }
+}
+
---
+++ program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MySecondProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How long should your password be?");
+        int userPasswordLength;
+        bool success = int.TryParse(Console.ReadLine(), out userPasswordLength);
+
+        if (success)
+        {
+            string userPasswordLengthString = userPasswordLength.ToString();
+            Console.WriteLine("The user password length is: " + userPasswordLengthString);
+
+            Password password = new Password();
+            string generatedPassword = password.passwordGen(userPasswordLength);
+            Console.WriteLine("Generated password: " + generatedPassword);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, please enter an integer.");
+        }
+       
+    }
+}
